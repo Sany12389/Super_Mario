5799efc65eb0e87e603a96605407e39446e024fb
Izabelly Márcia <santosizabelly6@gmail.com>
2025-06-02 14:07:00 -0300

    css

--- script.js
+++ script.js
@@ -10,29 +10,16 @@ setTimeout(() => {
 }
 
 const loop = setInterval(() => {
-    const pipe = document.querySelector('.pipe');
+
     const pipePosition = pipe.offsetLeft;
-    const marioPosition = mario.offsetBottom;
-    
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+
     console.log(pipePosition);
 
-    if (pipePosition <= 120) {
-        if (pipePosition > 0 && marioPosition < 80) {
-            // Game Over
-            pipe.style.animation = 'none';
-            pipe.style.left = `${pipePosition}px`;
-
-            mario.style.animation = 'none';
-            mario.style.bottom = `${marioPosition}px`;
-
-            mario.src = './images/game-over.png';
-            mario.style.width = '75px';
-            mario.style.marginLeft = '50px';
-
-            clearInterval(loop);
-        } else {
-            // Player is safe
-        }
+    if (pipePosition <= 120 && marioPosition < 80) {
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`;
+        
     }
 }, 10);
 
