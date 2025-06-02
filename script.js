0ec0f6cdac7ad7bcf992311badfd7e8c5a7aba5e
Izabelly Márcia <santosizabelly6@gmail.com>
2025-06-02 14:37:36 -0300

    pronto

--- script.js
+++ script.js
@@ -11,15 +11,23 @@ setTimeout(() => {
 
 const loop = setInterval(() => {
 
+    console.log('loop');
+
     const pipePosition = pipe.offsetLeft;
     const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
 
-    console.log(pipePosition);
 
-    if (pipePosition <= 120 && marioPosition < 80) {
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
         pipe.style.animation = 'none';
         pipe.style.left = `${pipePosition}px`;
+
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`;
         
+        mario.src = './img/game-over.png';
+        mario.style.width = '75px';
+        mario.style.marginLeft = '50px';
+        clearInterval(loop);
     }
 }, 10);
 
